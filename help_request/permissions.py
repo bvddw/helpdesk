434746ecdd7387ca1039e59f0f934be4c0f24b9d
bvddw <galaid.arseniy@gmail.com>
2023-10-18 04:25:51 +0300

    permission fixed

--- help_request/permissions.py
+++ help_request/permissions.py
@@ -1,13 +1,13 @@
-from rest_framework import permissions
+from rest_framework.permissions import BasePermission
 
 
-class RequesterOrReadOnly(permissions.BasePermission):
+class RequesterOrReadOnly(BasePermission):
     def has_permission(self, request, view):
         # Check if the user is authenticated
         return request.user.is_authenticated
 
     def has_object_permission(self, request, view, obj):
         # Allow request creators to set text and priority
-        if view.action == 'partial_update':
+        if view.action in ['partial_update', 'update', 'delete']:
             return request.user == obj.requester
         return request.user.is_superuser or request.user == obj.requester
